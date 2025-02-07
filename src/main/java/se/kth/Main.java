25fdb1a3e1c9c3de446d04cbb940fbc1c973ca9e
RasmussBrodin <rasmuss.brodin@gmail.com>
2025-02-07 14:22:10 +0100

    refractor: maven
    Make it a maven project. Closes #1

--- src/main/java/se/kth/Main.java
+++ src/main/java/se/kth/Main.java
@@ -1,6 +1,12 @@
+package se.kth;
+
 public class Main{
 
     public static void main(String[] args) {
         System.out.println("helloooo");
+    }
+
+    public static boolean test(){
+        return true;
     }
 }
